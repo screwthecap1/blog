912316ff05222b50880df6de6196d4994eb6a72d
screwthecap1 <chanskiy777@mail.ru>
2025-06-28 17:03:31 +0400

    Laravel Telescope: Optimizing DB queries

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,7 +12,8 @@ class Post extends Model
     protected $table = 'posts';
     protected $guarded = false;
 
-    protected $withCount=['likedUsers'];
+    protected $withCount = ['likedUsers'];
+    protected $with = ['category'];
 
     public function tags()
     {
